978c2c345cee55c7a2042be55a4a162da215fdfc
Leonard Chi <leonardochi168@gmail.com>
2022-09-22 15:30:38 +0100

    infix to posfix converter

--- ReversePolishNotation/RPN.py
+++ ReversePolishNotation/RPN.py
@@ -1,5 +1,26 @@
 from stacks import Stack
-operators = ['+', '-', '*', '/']
+operators = {'+':1, '-':1, '*':2, '/':2}
+
+def InToPos(Infix):
+    opstack = Stack(len(Infix))
+    result = ""
+    for c in Infix:
+        if c.isdigit():
+            result += c
+        elif c in operators:
+            if opstack.stptr != -1 and opstack.stack[opstack.stptr] in operators:
+                if operators[opstack.stack[opstack.stptr]] >= operators[c]:
+                    result += opstack.pop()
+            opstack.push(c)
+        elif c == '(':
+            opstack.push(c)
+        elif c == ')':
+            while opstack.stack[opstack.stptr] != '(':
+                result += opstack.pop()
+            opstack.pop()
+    while opstack.stptr != -1:
+        result += opstack.pop()
+    return result
 
 def calpostfix(eq):
     stack = Stack(10)
@@ -17,6 +38,8 @@ def calpostfix(eq):
 def test():
     postfix = ["34*9-", "512+4*3-+"]
     calpostfix(postfix[1])
+    postfix.append(InToPos("5+((1+2)*4)-3"))
+    calpostfix(postfix[2])
 
 def main():
     test()
